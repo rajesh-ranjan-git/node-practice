794188f6081126bd4606264bce4ca20edf76580e
Rajesh <rajeshranjan8271@gmail.com>
2024-11-21 04:01:09 +0530

    some changes

--- airbnbWithMVC/models/homeModel.js
+++ airbnbWithMVC/models/homeModel.js
@@ -3,30 +3,38 @@ import path from "path";
 import rootDir from "../utils/pathUtil.js";
 
 class HomeModel {
-    constructor(houseName, housePricePerNight, houseLocation, houseRating, housePhotoURL) {
-        this.houseName = houseName;
-        this.housePricePerNight = housePricePerNight;
-        this.houseLocation = houseLocation;
-        this.houseRating = houseRating;
-        this.housePhotoURL = housePhotoURL;
-    }
+  constructor(
+    id,
+    houseName,
+    housePricePerNight,
+    houseLocation,
+    houseRating,
+    housePhotoURL
+  ) {
+    this.id = id;
+    this.houseName = houseName;
+    this.housePricePerNight = housePricePerNight;
+    this.houseLocation = houseLocation;
+    this.houseRating = houseRating;
+    this.housePhotoURL = housePhotoURL;
+  }
 
-    save() {
-        HomeModel.fetchAllHomes(registeredHomes => {
-            registeredHomes.push(this);
-            const homeDataPath = path.join(rootDir, "data", "homes.json");
-            fs.writeFile(homeDataPath, JSON.stringify(registeredHomes), error => {
-                console.log("File writing concluded, ", error)
-            });
-        })
-    }
+  save() {
+    HomeModel.fetchAllHomes((registeredHomes) => {
+      registeredHomes.push(this);
+      const homeDataPath = path.join(rootDir, "data", "homes.json");
+      fs.writeFile(homeDataPath, JSON.stringify(registeredHomes), (error) => {
+        console.log("File writing concluded, ", error);
+      });
+    });
+  }
 
-    static fetchAllHomes(callback) {
-        const homeDataPath = path.join(rootDir, "data", "homes.json");
-        fs.readFile(homeDataPath, (err, data) => {
-            callback(!err ? JSON.parse(data) : []);
-        })
-    }
+  static fetchAllHomes(callback) {
+    const homeDataPath = path.join(rootDir, "data", "homes.json");
+    fs.readFile(homeDataPath, (err, data) => {
+      callback(!err ? JSON.parse(data) : []);
+    });
+  }
 }
 
 export default HomeModel;
